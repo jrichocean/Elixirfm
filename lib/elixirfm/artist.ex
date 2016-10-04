4653458b242c18a77c3d6e8c0242290dfcbc3a0c
jrichocean <jrichocean@gmail.com>
2016-10-04 13:25:11 -0600

    CREATES/ADDS basic tags module & minor fixes

--- lib/elixirfm/artist.ex
+++ lib/elixirfm/artist.ex
@@ -74,7 +74,7 @@ defmodule Elixirfm.Artist do
   end
 
 
-    def process_url(url), do: @endpoint_base <> url <> "&api_key=#{@api_key}&format=json"
-    def process_response_body(body), do: body |> Poison.decode!
+  def process_url(url), do: @endpoint_base <> url <> "&api_key=#{@api_key}&format=json"
+  def process_response_body(body), do: body |> Poison.decode!
 
 end
