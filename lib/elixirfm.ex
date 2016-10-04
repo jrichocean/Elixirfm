55217de9a5034497dbd0533fdc22301b74400748
jrichocean <jrichocean@gmail.com>
2016-10-04 12:51:16 -0600

    "ADDS/CREATES basic Lastfm album api coverage"

--- lib/elixirfm.ex
+++ lib/elixirfm.ex
@@ -5,18 +5,18 @@ defmodule Elixirfm do
   Currently in begining stages of development.  
   """
 
-  defmodule MissingSecretKeyError do
-    defexception message: """
-      The secret_key settings is required for some of the Lastfm endpoints. Please include your
-      strip api key in your application config file like so:
+  # defmodule MissingSecretKeyError do
+  #   defexception message: """
+  #     The secret_key settings is required for some of the Lastfm endpoints. Please include your
+  #     Last.fm api key in your application config file like so:
 
-        config :stripe, secret_key: YOUR_SECRET_KEY
+  #       config :elixirfm, secret_key: YOUR_SECRET_KEY
 
-      Alternatively, you can also set the secret key as an environment variable:
+  #     You can also set the secret key as an environment variable:
 
-        STRIPE_SECRET_KEY=YOUR_SECRET_KEY
-    """
-  end
+  #       LASTFM_SECRET_KEY=YOUR_SECRET_KEY
+  #   """
+  # end
 
 
 end
