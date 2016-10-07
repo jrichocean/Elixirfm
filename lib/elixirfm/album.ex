1a73567adb5621d227675018610ef30aa87bd988
jrichocean <jrichocean@gmail.com>
2016-10-06 21:08:13 -0600

    REMOVES :api_version from function args

--- lib/elixirfm/album.ex
+++ lib/elixirfm/album.ex
@@ -13,30 +13,30 @@ defmodule Elixirfm.Album do
   @doc """
   Search for an album by name. Returns album matches sorted by relevance.
   """
-  def search(query, args \\ [api_version: "2.0",limit: 5, page: 1]) do
-    Elixirfm.get_request(args[:api_version] <> "?method=album.search&album=#{query}")
+  def search(query, args \\ [limit: 5, page: 1]) do
+    Elixirfm.get_request("?method=album.search&album=#{query}")
   end
 
   @doc """
   Get Album info.
   """
-  def get_info(artist, album, args \\ [api_version: "2.0", username: "", lang: "eng"]) do
-    Elixirfm.get_request(args[:api_version] <> "/?method=album.getinfo&artist=#{artist}&album=#{album}&username=" <> args[:username] <> "&lang=" <> args[:lang])
+  def get_info(artist, album, args \\ [username: "", lang: "eng"]) do
+    Elixirfm.get_request("/?method=album.getinfo&artist=#{artist}&album=#{album}&username=" <> args[:username] <> "&lang=" <> args[:lang])
   end
 
   # @doc """
   # *requires auth
   # Gets an Albums tags applied by an individual. If not authenticated, must pass username
   # """
-  # def get_tags(artist, album, args \\ [api_version: "2.0"]) do
-  #   Elixirfm.get_request(args[:api_version] <> "/?method=album.gettags&artist=#{artist}&album=#{album}")
+  # def get_tags(artist, album, args \\ []) do
+  #   Elixirfm.get_request("/?method=album.gettags&artist=#{artist}&album=#{album}")
   # end
 
   @doc """
   Get the top tags for an album on Last.fm, ordered by popularity.
   """
-  def get_top_tags(artist, album, args \\ [api_version: "2.0"]) do
-    Elixirfm.get_request(args[:api_version] <> "/?method=album.gettoptags&artist=#{artist}&album=#{album}")
+  def get_top_tags(artist, album, args \\ []) do
+    Elixirfm.get_request("/?method=album.gettoptags&artist=#{artist}&album=#{album}")
   end
 
 end
--- lib/elixirfm/chart.ex
+++ lib/elixirfm/chart.ex
@@ -7,22 +7,22 @@ defmodule Elixirfm.Chart do
   @doc """
   Get the top artists chart
   """
-  def get_top_artists(args \\ [api_version: "2.0", page: 1, limit: 30]) do
-    Elixirfm.get_request(args[:api_version] <> "/?method=chart.gettopartists&page=#{args[:page]}&limit=#{args[:limit]}")
+  def get_top_artists(args \\ [page: 1, limit: 30]) do
+    Elixirfm.get_request("/?method=chart.gettopartists&page=#{args[:page]}&limit=#{args[:limit]}")
   end
 
   @doc """
   Get the top tags chart
   """
-  def get_top_tags(args \\ [api_version: "2.0", page: 1, limit: 30]) do
-    Elixirfm.get_request(args[:api_version] <> "/?method=chart.gettoptags&page=#{args[:page]}&limit=#{args[:limit]}")
+  def get_top_tags(args \\ [page: 1, limit: 30]) do
+    Elixirfm.get_request("/?method=chart.gettoptags&page=#{args[:page]}&limit=#{args[:limit]}")
   end
 
   @doc """
   Get the top tracks chart
   """
-  def get_top_tracks(args \\ [api_version: "2.0", page: 1, limit: 30]) do
-    Elixirfm.get_request(args[:api_version] <> "/?method=chart.gettoptracks&page=#{args[:page]}&limit=#{args[:limit]}")
+  def get_top_tracks(args \\ [page: 1, limit: 30]) do
+    Elixirfm.get_request("/?method=chart.gettoptracks&page=#{args[:page]}&limit=#{args[:limit]}")
   end
 
 end
