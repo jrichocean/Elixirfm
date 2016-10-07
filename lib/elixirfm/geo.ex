578c4e2451091b2df079ff7a89f7b0567ce5a7ee
jrichocean <jrichocean@gmail.com>
2016-10-06 21:57:04 -0600

    ADDS Elixirfm.Geo module

---
+++ lib/elixirfm/geo.ex
@@ -0,0 +1,22 @@
+defmodule Elixirfm.Geo do
+  @moduledoc """
+  Last.fm Geo endpoint api
+  """
+
+  @doc """
+  Get the most popular artists on Last.fm by country.
+
+  ```query``` must be a country name, as defined by the [ISO 3166-1](http://www.nationsonline.org/oneworld/country_code_list.htm) country names standard.
+  """
+  def get_top_artists(query, args \\ [limit: 20, page: 1]) do
+    Elixirfm.get_request("?method=geo.gettopartists&country=#{query}&limit=#{args[:limit]}&page=#{args[:page]}")
+  end
+
+  @doc """
+  Get the most popular tracks on Last.fm last week by country.
+  """
+  def get_top_tracks(query, args \\ [page: 1, limit: 10, location: ""]) do
+    Elixirfm.get_request("?method=geo.gettoptracks&country=#{query}&page=#{args[:page]}&limit=#{args[:limit]}&location=#{args[:location]}")
+  end
+
+end
