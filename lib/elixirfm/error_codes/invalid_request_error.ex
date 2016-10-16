bf27484aa10a80278f3579aafa5b3de177df3a3a
jrichocean <jrichocean@gmail.com>
2016-10-16 14:10:38 -0600

    CREATES/ADDS InvalidRequestError module to /error_codes

---
+++ lib/elixirfm/error_codes/invalid_request_error.ex
@@ -0,0 +1,6 @@
+defmodule Elixirfm.InvalidRequestError do
+  @moduledoc """
+  Invalid request errors arise when your request has invalid parameters.
+  """
+  defstruct type: "invalid_request_error", message: nil, param: nil
+end
