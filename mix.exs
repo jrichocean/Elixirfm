e18355ae7b8d92023bfeba27147745fe31ece5c3
joshua.richardson <jrichocean@gmail.com>
2017-04-17 11:19:58 -0600

    UPDATES version and readme.md

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elixirfm.Mixfile do
   def project do
     [
       app: :elixirfm,
-      version: "0.0.8",
+      version: "0.0.9",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
