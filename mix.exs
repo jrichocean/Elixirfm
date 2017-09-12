4abc8ca19260304f4e5ec6c919b63b3b9837ce9f
joshua.richardson <jrichocean@gmail.com>
2017-09-12 08:50:26 -0600

    UPDATES elixir version and deps

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Elixirfm.Mixfile do
     [
       app: :elixirfm,
       version: "0.0.10",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
@@ -47,11 +47,11 @@ defmodule Elixirfm.Mixfile do
       {:credo, "~> 0.6", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:exvcr, "~> 0.8", only: :test},
-      {:httpoison, "~> 0.11.1"},
+      {:httpoison, "~> 0.13.0"},
       {:poison, "~> 3.1"},
 
       # Documentation
-      {:ex_doc, "~> 0.15.1", only: :dev},
+      {:ex_doc, "~> 0.16.0", only: :dev},
       {:inch_ex, "~> 0.5", only: [:dev, :test]}
     ]
   end
