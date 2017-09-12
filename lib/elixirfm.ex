c28b385bfbd97e92ebcf503e5418c0b848cce3f4
joshua.richardson <jrichocean@gmail.com>
2017-09-12 10:44:03 -0600

    CLEANS up code

--- lib/elixirfm.ex
+++ lib/elixirfm.ex
@@ -14,18 +14,18 @@ defmodule Elixirfm do
   @api_root "http://ws.audioscrobbler.com/"
 
   @doc false
-  def lastfm_key do
+  def lastfm_key() do
     Application.get_env(:elixirfm, :api_key, System.get_env("LASTFM_API_KEY"))
     || raise MissingApiKeyError
   end
 
   @doc false
-  def lastfm_secret do
+  def lastfm_secret() do
     Application.get_env(:elixirfm, :secret_key, System.get_env("LASTFM_SECRET_KEY"))
     || raise MissingSecretKeyError
   end
 
-  @spec get_request(String.t()) :: {atom(), term()}
+  @spec get_request(String.t(), keyword()) :: {atom(), term()}
   def get_request(endpoint, opts \\ [api_version: "2.0"]) do
     HTTPoison.request(:get, request_url(endpoint, opts[:api_version]), [], create_headers())
     |> handle_response()
@@ -35,10 +35,7 @@ defmodule Elixirfm do
     @api_root <> "#{opts}/?method=" <> endpoint <> "&api_key=" <> lastfm_key() <> "&format=json"
   end
 
-  defp create_headers do
-    app_version = Mix.Project.config[:version]
-    [{"Authorization", "Bearer #{lastfm_key()}"}]
-  end
+  defp create_headers(), do: [{"Authorization", "Bearer #{lastfm_key()}"}]
 
   @spec handle_response({atom(), map()}) :: tuple()
   defp handle_response({:error, struct}), do: {:error, "There was an error", struct}
@@ -49,6 +46,6 @@ defmodule Elixirfm do
     {:error, error_struct}
   end
 
-  defp process_response_body(body), do: body |> Poison.decode!()
+  defp process_response_body(body), do: Poison.decode!(body)
 
 end
