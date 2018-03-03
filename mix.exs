52d98b4006524d972540b64026758c1a37998bb4
joshua.richardson <jrichocean@gmail.com>
2018-03-03 16:50:05 -0700

    UPDATE patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elixirfm.Mixfile do
   def project do
     [
       app: :elixirfm,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
